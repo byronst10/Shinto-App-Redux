7c7fdcd5a5b777d88e20cd55ed0f0a0615fbd5d4
Byron Tottaram <53205511+byronst10@users.noreply.github.com>
2020-04-28 23:02:09 -0400

    Backend (#2)
    * pushing initial backend files, mongoDB setup successfully
    
    * completed user routes, working on kamvas routes

---
+++ backend/models/kamvas.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const kamvasSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    description: { type: String, required: true }
+    // duration: { type: Number, required: true },
+    // date: { type: Date, required: true }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Kamvas = mongoose.model("Kamvas", kamvasSchema);
+
+module.exports = Kamvas;
